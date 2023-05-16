b20c534da688ba93e44b23c3d42c9a9da38b56e7
Ionut Ciovica <ionut.ciovica@gmail.com>
2023-05-16 20:35:16 +0100

    Initial Commit

---
+++ Secret-Life/Secret_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secret_LifeApp.swift
+//  Secret-Life
+//
+//  Created by IonutCiovica on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Secret_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
